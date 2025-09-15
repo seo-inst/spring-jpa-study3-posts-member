dd7b1cbee032c0671f8d9ed1b1f20158883e4e06
seo-inst <innerpeace.deepi@gmail.com>
2025-09-15 10:21:09 +0900

    Initial commit

---
+++ src/main/java/org/kosa/myproject/SpringJpaStudy3PostsMemberApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaStudy3PostsMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaStudy3PostsMemberApplication.class, args);
+	}
+
+}
